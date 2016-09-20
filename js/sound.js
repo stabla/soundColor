ffd4eb9a82bdff6d0fc3aaacfa88e2dbc6fd1283
Guillaume Stabla <idkn@users.noreply.github.com>
2016-09-20 22:52:03 +0200

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -51,7 +51,7 @@ if (isIE) {
 //      colorModifiers[$(this)[0].id] = $(this).val();
 //    });
 
-    d.getElementsByClassName('inputDiv').getElementsByTagName('input').addEventListener('input', function() {
+    d.querySelectorAll('.inputDiv input').addEventListener('input', function() {
         this.parentElement.previousElementSibling.querySelectorAll('span').textContent(this.valueOf);
         colorModifiers[this[0].id] = this.valueOf;
     }).forEach(function() {
